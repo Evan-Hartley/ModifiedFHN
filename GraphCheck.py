eb5a36b8f27965e8df63c582aa9f2fd2e6bc3751
Evan-Hartley <evan.h.rheaume@gmail.com>
2025-06-06 14:22:39 -0400

    Add files via upload

---
+++ GraphCheck.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import time
+
+def plot_heatmap(u, v, t, params, ax=None, colors_volt='RdYlBu_r'):
+    if ax is None:
+        fig, ax = plt.subplots()  # create new axis if not provided
+        created_fig = True
+    else:
+        created_fig = False
+
+    heatmap = ax.imshow(u, cmap=colors_volt, origin='lower', vmin=-0.4, vmax=1.2)
+    plt.colorbar(heatmap, location='left', ax=ax, fraction=0.046, pad=0.04, label="Normalized Voltage")
+
+    ax.set_title(f'Voltage Map at {round(round(t) * params.dt,2)} ms', fontsize=24)
+    ax.set_xlabel('x-space', fontsize=16)
+    ax.set_ylabel('y-space', fontsize=16)
+    ax.axis('off')
+
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    plt.savefig("VoltMap" + timestr + ".png")
+
+    # Clear or close figure
+    if created_fig:
+        plt.close()  # if we created the figure, just close it
+    else:
+        ax.clear()   # if user passed in an ax, just clear the axis
